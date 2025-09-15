b1e24d5720566970667d279d227ff4008b55ce3f
user <user@DESKTOP-ILE6SAL>
2025-09-15 22:15:17 +0200

    add the controllers and created the functionality between them all

---
+++ ProgPart1/Controllers/AddClaimController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgPart1.Controllers
+{
+    public class AddClaimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+           
+            return View("ClaimSubmitted");
+        }
+
+       
+    }
+}
---
+++ ProgPart1/Controllers/ViewClaimController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgPart1.Controllers
+{
+    public class ViewClaimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Delete() 
+        { 
+            return View("ClaimDeleted");
+        }
+    }
+}
